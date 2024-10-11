e8662f08d0a5f266912263b980e0db26a5176526
Packss <enzoana@hotmail.com>
2024-10-11 17:59:33 -0300

    Update ecwrite.py to handle loading acpi_ec module if ec_path is not found

--- ecwrite.py
+++ ecwrite.py
@@ -1,4 +1,5 @@
 import os
+from PyQt5.QtCore import QProcess
 # EC_IO_FILE = '/sys/kernel/debug/ec/ec0/io'
 EC_IO_FILE = '/dev/ec' if os.system('ls /sys/kernel/debug/ec/ec0/io 2> /dev/null > /dev/null') else '/sys/kernel/debug/ec/ec0/io'
 
@@ -14,18 +15,23 @@ class ECWrite:
 
     def setupEC(self):
         try:
-            self.ec_file = open(self.ec_path, 'rb+')#, buffering=0)
+            self.ec_file = open(self.ec_path, 'rb+')  # buffering=0)
         except PermissionError:
             print('Run with sudo')
             exit(1)
         except FileNotFoundError:
             print(self.ec_path, 'not found. Check acpi_ec')
-            exit(1)
-            # from subprocess import Popen
-            # Popen(['modprobe', 'ec_sys', 'write_support=1'])
-            # print('EC Changed. Restarting the application may help if it is not working.')
+            process = QProcess()
+            process.start('modprobe acpi_ec')
+            process.waitForFinished()
+            process.close()
+            try:
+                self.ec_file = open(self.ec_path, 'rb+')
+            except FileNotFoundError:
+                print('Failed to load acpi_ec')
+                exit(1)
         except Exception as e:
-            print("Error: " + str(e))       
+            print("Error: " + str(e))
 
     def ec_write(self, address, value):
 
@@ -45,15 +51,15 @@ class ECWrite:
             # print(self.buffer)
             if self.buffer == b'':
                 print("BAD BUFFER EXITING!")
-                exit(1) 
-            
+                exit(1)
+
         except Exception as e:
             print("Error: " + str(e))
-            exit(1)           
+            exit(1)
 
     ## Read EC contents from buffer instead of going to disk
     def ec_read(self, address):
-        try:        
+        try:
             # self.ec_file.seek(address)
             # return ord(self.ec_file.read(1))
 
@@ -65,7 +71,7 @@ class ECWrite:
             # return ord(self.buffer[int(address)])
         except Exception as e:
             print("Error: " + str(e))
-            exit(1)            
+            exit(1)
 
     def shutdownEC(self):
         self.ec_file.close()
